cf47a6bf5b1372baa054518067db6b69641ed5a3
Christian Romney <cromney@pointslope.com>
2012-08-07 17:38:47 -0400

    Added mode
    Signed-off-by: Christian Romney <cromney@pointslope.com>

--- stats.py
+++ stats.py
@@ -1,4 +1,5 @@
 import math
+import operator
 
 class Stats(object):
     def __init__(self, d):
@@ -29,9 +30,13 @@ class Stats(object):
         """Return a frequency map of the data"""
         if (not self.freq) and self.length():
             for i, x in enumerate(self.data):
-                self.freq[x] = self.freq.get(x, 0) + 1
+                self.freq[x] = self.frequency(x) + 1
         return self.freq.copy()
-
+    
+    def frequency(self, value):
+        """Return the frequency of a given value"""
+        return self.freq.get(value, 0)
+        
     # TODO: reimplement using map()
     def pmf(self):
         """Return the probability mass function of the data"""
@@ -40,13 +45,22 @@ class Stats(object):
                 self.mass[k] = v / float(self.length())
         return self.mass.copy()
 
+    def mode(self):
+        """Return the mode of the data set"""
+        key, value = max(self.freq.iteritems(), key=operator.itemgetter(1))
+        return key
+
 # Test things out
 if __name__ == "__main__":
     data = (2, 3, 9, 2, 3, 9, 2, 1, 0, 1, 2, 0, 2)
     stats = Stats(data)
-    print stats.frequencies()
-    print stats.pmf()
-    print sum(stats.pmf().values())
-    print stats.mean()
-    print stats.variance()
-    print stats.stdev()
+
+    print "Frequencies:", stats.frequencies()
+    print "Probability Mass Function:", stats.pmf()
+    print "Sum of probabilities: %.1f" % sum(stats.pmf().values())
+    print "Arithmetic Mean: %d" % stats.mean()
+    print "Variance: %.2f" % stats.variance()
+    print "Standard deviation: %.2f" % stats.stdev()
+    print "Frequency of '2': %d" % stats.frequency(2)
+    print "Mode: %d" % stats.mode()
+
